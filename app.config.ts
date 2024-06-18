d7ef4a3eb1f15af2f72fe92d6016dc35860a35db
Smashinfries <jus10luka@gmail.com>
2024-06-18 02:07:06 -0500

    config update

--- app.config.ts
+++ app.config.ts
@@ -22,7 +22,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
 	android: {
 		adaptiveIcon: {
 			foregroundImage: './assets/adaptive-icon.png',
-			backgroundColor: '#ffffff',
+			// backgroundColor: '#ffffff',
 		},
 		package: IS_DEV ? 'com.vnbrowserdev' : 'com.vnbrowser',
 		intentFilters: [
